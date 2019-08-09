6a93db893cb21c3011de6053a6df3aca87f68e87
liyang <7325560@qq.com>
2019-08-09 11:11:33 +0800

    fix ~ sign in install script with $HOME.

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-TARGET_DIR="~/Library/Rime/"
+TARGET_DIR="$HOME/Library/Rime"
 
 if [ ! -z $TARGET_DIR ]; then
-    GIT_DIR="~/.dot-squirrel"
+    GIT_DIR="$HOME/.dot-squirrel"
 
     if [ -z $GIT_DIR ]; then
         git clone github:wood9366/dot-squirrel $GIT_DIR
     fi
 
-    ln -s $GID_DIR/default.custom.yaml $TARGET_DIR
-    ln -s $GID_DIR/luna_pinyin_simp.custom.yaml $TARGET_DIR
+    ln -s $GIT_DIR/default.custom.yaml $TARGET_DIR/
+    ln -s $GIT_DIR/luna_pinyin_simp.custom.yaml $TARGET_DIR/
 else
     echo "no squirrel found"
 fi
